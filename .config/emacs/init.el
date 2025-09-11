8549f6d61a30ca4fe42cbedff132b13b17a379a6
NU11V01D <paul.alvarez.ramos@icloud.com>
2025-09-11 15:49:32 -0600

    modified default color scheme and added a (non-functional) function to toggle relative line numbers.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,28 +5,29 @@
 (package-initialize)
 
 ;;; Options
+(global-display-line-numbers-mode 1)
 ;; Basic
-(tool-bar-mode -1) ;; Disable default Emacs Toolbar.
-(menu-bar-mode -1) ;; Disable default Emacs Menubar.
-(delete-selection-mode t) ;; Something something deletes something.
-(setq custom-file (make-temp-file "EmacsCustom")) ;; Make a temp-file to save the annoying custom files.
-(setq inhibit-startup-screen t) ;; Disable default Emacs startup screen.
-;; Editor
 (setq scroll-conservatively 101)
-(setq display-line-numbers t)
-(setq display-line-numbers-type 'relative)
 
 ;;; Keymaps
 
 
 ;;; Packages
 (use-package kanagawa-themes
+  :ensure t)
+(use-package doom-themes
   :ensure t
   :config
-  (load-theme 'kanagawa-wave t))
-(use-package doom-themes
-  :ensure t)
+  (load-theme 'doom-one))
 (use-package doom-modeline
   :ensure t
   :init
   (doom-modeline-mode 2))
+
+;;; Custom Functions
+(defun my/toggle-relative-numbers ()
+  "Toggles between relative and absolute line numbering."
+  (interactive)
+  (if (eq display-line-numbers-type 'relative)
+      (setq display-line-numbers-type t) ;; Absolute line numbers
+    (setq display-line-numbers-type 'relative))) ;; Relative line numbers otherwise
