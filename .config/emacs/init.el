c504179d23781a603dc825b185e9562534e666b2
NU11V01D <paul.alvarez.ramos@icloud.com>
2025-09-11 12:32:30 -0600

    emacs complained about using comments incorrectly so I fixed it and dumped a few plugins while I was at it.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,15 +5,28 @@
 (package-initialize)
 
 ;;; Options
-(setq inhibit-startup-screen t "Disable startup scree.")
-(setq scroll-conservatively 101 "Smooth scrolling")
-(setq display-line-numbers t "Show line numbers")
-(setq display-line-numbers-type 'relative "Show relative line numbers")
-(setq-default cursor-line t "Activates the cursorline")
+;; Basic
+(tool-bar-mode -1) ;; Disable default Emacs Toolbar.
+(menu-bar-mode -1) ;; Disable default Emacs Menubar.
+(delete-selection-mode t) ;; Something something deletes something.
+(setq custom-file (make-temp-file "EmacsCustom")) ;; Make a temp-file to save the annoying custom files.
+(setq inhibit-startup-screen t) ;; Disable default Emacs startup screen.
+;; Editor
+(setq scroll-conservatively 101)
+(setq display-line-numbers t)
+(setq display-line-numbers-type 'relative)
 
 ;;; Keymaps
 
 
 ;;; Packages
-
-
+(use-package kanagawa-themes
+  :ensure t
+  :config
+  (load-theme 'kanagawa-wave t))
+(use-package doom-themes
+  :ensure t)
+(use-package doom-modeline
+  :ensure t
+  :init
+  (doom-modeline-mode 2))
